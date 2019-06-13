da10bbbfe966299062ce6041c81557012232eee2
chbmb <chbmb@linuxserver.io>
2019-06-13 23:56:00 +0100

    Initial Dump
    I've done a README, added a GPLv3 License, and uploaded all the existing builds to the repository, as well as compiled v6.7.1rc2 to test the script.  I've added a cleanup routine to the beginning of the script to remove any leftover files.

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# rmrr-unraid
+# Unraid HP Proliant Edition
+
+## Introduction
+
+It is well documen﻿ted that HP Proliant servers have RMRR issues using certain BIOS versions after about 2011 when trying to passthrough devices in a linux environment. Device passthrough fails and the onscreen error will show:
+
+`vfio: failed to set iommu for container: Operation not permitted`
+
+And a look further into the logs show:
+
+`Device is ineligible for IOMMU domain attach due to platform RMRR requirement.  Contact your platform vendor.`
+
+HP is aware of this problem and is not updating older machines. There are some bios options to try to fix this on newer models with some success.
+
+This script will compile a new patched version of Unraid which bypasses the RMRR check.
+
+## Usage
+
+It is recommended to compile this in a **dedicated** folder, preferably on a cache device.
+
+```
+mkdir -p /mnt/cache/.rmrr
+cd /mnt/cache/.rmrr
+wget https://raw.githubusercontent.com/AnnabellaRenee87/Unraid-HP-Proliant-Edition/master/build_script/kernel-compile.sh
+chmod +x kernel-compile.sh
+./kernel-compile.sh
+```
+
+A new bzimage file is then compiled with the RMRR patch applied.
+
+## Support
+
+Discussion about this patch and it's use on Unraid should be directed to the [thread on the Unraid forums](https://forums.unraid.net/topic/72681-unraid-hp-proliant-edition-rmrr-error-patching/?tab=comments#comment-668032&searchlight=1).
+
+## Dependencies
+
+Dependencies are found in the [Unraid-Dependencies Repository](https://github.com/linuxserver/Unraid-Dependencies)
+
+## Credits 
+
+Maintainers: [AnnabelleRenee87](https://forums.unraid.net/profile/73615-annabellarenee87/) & [1812](https://forums.unraid.net/profile/70493-1812/)
+
+This Unraid edition is based on the work of several others, and full credit is due to them.
+
+First is the source of the patch code itself from the Promox forum users [here](https://forum.proxmox.com/threads/tutorial-compile-proxmox-ve-5-with-patched-intel-iommu-driver-to-remove-rmrr-check.36374/) & [here](https://forum.proxmox.com/threads/hp-proliant-microserver-gen8-raidcontroller-hp-p410-passthrough-probleme.30547/).
+
+The second is [kabloomy](https://forums.unraid.net/profile/75139-kabloomy/) who successfully created a working edition using a modified DVB edition script on Unraid 6.4.1.
+
+And last but not least, is the modified script currently in use provided by the totally awesome [CHBMB](https://github.com/CHBMB) (Thank you!!!)
---
+++ build_script/kernel_compile.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+##Cleanup any old files before starting
+rm -rf ${D}/kernel
+rm \
+  FILE_LIST_CURRENT \
+  linux-*.tar.xz \
+  variables.sh \
+  URLS_CURRENT
+
+##Set branch to pull from for dependencies
+set -ea
+
+: "${DEPENDENCY_BRANCH:=master}"
+
+##Pull variables from github
+wget -nc https://raw.githubusercontent.com/linuxserver/Unraid-Dependencies/${DEPENDENCY_BRANCH}/build_scripts/variables.sh
+
+source ./variables.sh
+
+if [[ -z "$D" ]]; then
+    echo "Must provide D in environment" 1>&2
+    exit 1
+fi
+
+source ${D}/dvb-variables.sh
+
+##Grab Slackware packages
+echo -e "${BLUE}Kernel Compile Module${NC}    -----    Install packages"
+[ ! -d "${D}/packages" ] && mkdir ${D}/packages
+  wget -nc -P ${D}/packages -i ${D}/URLS_CURRENT
+  if [[ $? != 0 ]]; then
+    echo "Package missing. Exiting..."
+    exit 1
+  fi
+
+#Pull static gcc deps
+  wget -nc -P ${D}/packages https://github.com/linuxserver/Unraid-Dependencies/raw/${DEPENDENCY_BRANCH}/gcc/gcc-8.3.0-x86_64-1.txz
+  wget -nc -P ${D}/packages https://github.com/linuxserver/Unraid-Dependencies/raw/${DEPENDENCY_BRANCH}/gcc/gcc-g++-8.3.0-x86_64-1.txz
+
+##Install packages  
+installpkg ${D}/packages/*.*
+
+#Change to current directory
+echo -e "${BLUE}Kernel Compile Module${NC}    -----    Change to current directory"
+cd ${D}
+
+##Download and Install Kernel
+echo -e "${BLUE}Kernel Compile Module${NC}    -----    Download and Install Kernel"
+[[ $(uname -r) =~ ([0-9.]*) ]] &&  KERNEL=${BASH_REMATCH[1]} || return 1
+  LINK="https://www.kernel.org/pub/linux/kernel/v4.x/linux-${KERNEL}.tar.xz"
+  rm -rf ${D}/kernel; mkdir ${D}/kernel
+  [[ ! -f ${D}/linux-${KERNEL}.tar.xz ]] && wget $LINK -O ${D}/linux-${KERNEL}.tar.xz
+  tar -C ${D}/kernel --strip-components=1 -Jxf ${D}/linux-${KERNEL}.tar.xz
+  rsync -av /usr/src/linux-$(uname -r)/ ${D}/kernel/
+  cd ${D}/kernel
+
+  for p in $(find . -type f -iname "*.patch"); do patch -N -p 1 < $p
+  done
+  make oldconfig
+
+##Make necessary changes to fix HP RMRR issues
+cd $D
+sed -i '/return -EPERM;/d' $D/kernel/drivers/iommu/intel-iommu.c
+
+##Compile Kernel
+echo -e "${BLUE}Kernel Compile Module${NC}    -----    Compile Kernel"
+cd ${D}/kernel
+make -j $(grep -c ^processor /proc/cpuinfo)
+
+#Package Up new bzimage
+echo -e "${BLUE}Kernel Compile Module${NC}    -----    Package Up new bzimage"
+mkdir -p ${D}/${UNRAID_VERSION}
+cp -f ${D}/kernel/arch/x86/boot/bzImage ${D}/${UNRAID_VERSION}/bzimage
+
+##Calculate sha256 on new bzimage
+echo -e "${BLUE}Kernel Compile Module${NC}    -----    Calculate sha256 on new bzimage"
+cd ${D}/${UNRAID_VERSION}/
+sha256sum bzimage > bzimage.sha256
+
+##Return to original directory
+echo -e "${BLUE}Kernel Compile Module${NC}    -----    Return to original directory"
+cd ${D}
+  
